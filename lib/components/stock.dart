e9cc4b108537135e99567ecd01e2c6340a8cab6f
Elsa Zheng <zhengelsa150@gmail.com>
2026-09-14 19:40:22 -0700

    Rendering
    Make the card able to be seen

--- lib/components/stock.dart
+++ lib/components/stock.dart
@@ -2,7 +2,7 @@ import 'package:flame/components.dart';
 
 /*
     * PositionComponent is a component that has a position and size
- */
+*/
 class Stock extends PositionComponent{
   @override
   bool get debugMode => true; //turned on debug mode to view
